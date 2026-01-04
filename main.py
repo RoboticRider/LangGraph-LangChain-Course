984f09771cd1dcd04a714eebc2d10619917d3620
Jai <jairpaone@gmail.com>
2026-01-04 17:37:10 +0530

    Added Info finder instead of info summarizer

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ load_dotenv()
 def main():
     print("Hello from Information Summarizer Agent")
 
-    information = "Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2021; as of December 2025, Forbes estimates his net worth to be around US$754 billion."
+    name = "Elon Reeve Musk"
 
-    summary_template="""Given the following {information} about a person, generate a concise summary highlighting their key achievements and roles. Give the Output in Points."""
+    summary_template="""Given the following {name}, generate a concise summary highlighting their key achievements and roles. Give the Output in Points, max 3 points."""
 
-    summary_prompt_template=PromptTemplate(input_variables=["information"] ,template=summary_template)
+    summary_prompt_template=PromptTemplate(input_variables=["name"] ,template=summary_template)
 
-    llm = ChatOpenAI(model_name="gpt-5", temperature=0)
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
 
     chain = summary_prompt_template | llm
-    response = chain.invoke({"information" : information})
+    response = chain.invoke({"name" : name})
 
     print(response.content)
 
