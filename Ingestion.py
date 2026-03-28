6902dc43592d39577923026c020a1670f3a0c8e6
Jai <jairpaone@gmail.com>
2026-03-28 11:00:58 +0530

    1st commit

---
+++ Ingestion.py
@@ -0,0 +1,27 @@
+import asyncio
+import os
+import ssl
+from typing import Any, Dict, List
+
+import certifi
+from dotenv import load_dotenv
+from langchain_core.documents import Document
+from langchain_ollama import OllamaEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_tavily import tavily_crawl, tavily_map, tavily_search
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# Configure SSL context to use certifi certificates
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+load_dotenv()
+
+
+def main():
+    print("In Ingestion Pipeline File")
+
+
+if __name__ == "__main__":
+    main()
