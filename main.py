cd73d510cb04a748de8df3e48029ec90ed9bcb29
Jai <jairpaone@gmail.com>
2026-03-29 18:25:32 +0530

    2nd commit

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from crewai import Agent, Task, Crew
+from crewai import Agent, Task, Crew, LLM
 
-app = FastAPI()
-security = HTTPBearer()
-
-
-# 🔹 CrewAI logic (STATIC INPUT)
 def main():
 
-    user_input = "Build an invoice processing automation using UiPath that reads invoices from email, extracts data, and updates ERP system"
+    user_input = "Explain Machine Learning and summarize it"
 
     researcher = Agent(
         role="Automation Architect",
         goal="Understand requirement and design automation solution",
         backstory="Expert in UiPath and intelligent automation",
         verbose=True,
-        llm="ollama/qwen3.5:0.8b"
+        llm=LLM(model="ollama/qwen3:0.6b", temperature=0)
     )
 
     task = Task(
         description=f"""
-        User Requirement:
-        {user_input}
+        Give me more information on:
+        {user_input} and Summarize it as well.
 
         Generate:
-        - Step-by-step UiPath workflow
+        - Bullet points
         - Clear structured output
         """,
-        expected_output="Detailed structured automation steps",
+        expected_output="Summary generated for the given task in description",
         agent=researcher
     )
 
@@ -43,16 +36,6 @@ def main():
     print("\n🔥 AGENT OUTPUT:\n")
     print(result)
 
-    return result
 
-
-# ✅ STATIC API (no user input required)
-@app.get("/run")
-def run_agent(credentials: HTTPAuthorizationCredentials = Depends(security)):
-
-    if credentials.credentials != "my-secret-token":
-        raise HTTPException(status_code=401, detail="Unauthorized")
-
-    result = main()
-
-    return {"output": str(result)}
+if __name__ == "__main__":
+    main()
